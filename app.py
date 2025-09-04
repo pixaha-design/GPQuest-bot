8eb10a653ec1811c50cf20eb982d5af7d9f84f24
pixaha-design <pixaha@gmail.com>
2025-09-04 18:41:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,6 @@ def webhook():
     return {"ok": True}
 
 if __name__ == "__main__":
-    # Запускаем сервер локально
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
