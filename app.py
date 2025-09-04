f74430f32b82cb1d2f061078533ff13a044456c8
pixaha-design <pixaha@gmail.com>
2025-09-04 20:26:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+  #  import os
+  #  port = int(os.environ.get("PORT", 10000))
+  #  app.run(host="0.0.0.0", port=port)
+
 
